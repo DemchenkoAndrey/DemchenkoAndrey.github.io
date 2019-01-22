9f9949acfd1b612c72ac812568cb02b261094939
andriidemchenko <demchenko.andrey@gmail.com>
2019-01-22 12:15:04 +0300

    add jshint config file

--- script.js
+++ script.js
@@ -25,8 +25,8 @@ function chooseExpenses() {
 		let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
 			b = prompt("Во сколько обойдется?", '');
 	
-		if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-			&& a != '' && b != '' && a.length < 50) {
+		if ((typeof(a)) === 'string' && (typeof(a)) != null &&
+		 	(typeof(b)) != null && a != '' && b != '' && a.length < 50) {
 			console.log('Done');
 			appData.expenses[a] = b;
 		} else {
