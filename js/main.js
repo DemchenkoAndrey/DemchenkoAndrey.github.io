e8c328d32f588f6ea1bc7bc510e82b57c657f37a
andriidemchenko <demchenko.andrey@gmail.com>
2019-01-25 17:03:26 +0300

    add template

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let start = document.getElementById('start'),
+    budgetValue = document.querySelector('.budget-value'),
+    daybudgetValue = document.querySelector('.daybudget-value'),
+    levelValue = document.querySelector('.level-value'),
+    expensesValue = document.querySelector('.expenses-value'),
+    optionalexpensesValue = document.querySelector('.optionalexpenses-value'),
+    incomeValue = document.querySelector('.income-value'),
+    monthsavingsValue = document.querySelector('.monthsavings-value'),
+    yearsavingsValue = document.querySelector('.yearsavings-value'),
+    expensesInputs = document.querySelectorAll('.main .data .expenses-item');
+console.log(expensesInputs);
+
